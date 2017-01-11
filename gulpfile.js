0c5b67c70b55ec386c8318f75e60e9fcd94d4931
iainjmitchell <iainjmitchell@gmail.com>
2017-01-11 20:36:04 +0000

    adding sass and starting to add page metadata

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp')
 const s3_user = require('./aws_security.json')
 const awspublish = require('gulp-awspublish')
+const sass = require('gulp-sass')
+const w3cjs = require('gulp-w3cjs')
 
-gulp.task('deploy', ()=> {
+gulp.task('deploy', ['sass', 'w3c-validation'], ()=> {
   const publisher = awspublish.create({
     region: 'eu-west-2',
     params: {
@@ -10,12 +12,24 @@ gulp.task('deploy', ()=> {
     },
     accessKeyId: s3_user.key,
     secretAccessKey: s3_user.secret
-  });
-  var headers = {
+  })
+  const headers = {
     'Cache-Control': 'max-age=315360000, no-transform, public'
-  };
+  }
   return gulp.src('public/**/*')
     .pipe(publisher.publish(headers))
     .pipe(publisher.sync())
-    .pipe(awspublish.reporter());
+    .pipe(awspublish.reporter())
+})
+
+gulp.task('sass', () => {
+  return gulp.src(['./sass/*.scss'])
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'))
+})
+ 
+gulp.task('w3c-validation', () =>  {
+    gulp.src('public/*.html')
+        .pipe(w3cjs())
+        .pipe(w3cjs.reporter());
 })
