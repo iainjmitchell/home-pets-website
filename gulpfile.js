f8dd83d0a07b239f271805e6e197fda8829ab0ad
iainjmitchell <iainjmitchell@gmail.com>
2017-01-16 08:27:59 +0000

    adding mx record for email, work on adding header across all pages

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,21 @@ const s3_user = require('./aws_security.json')
 const awspublish = require('gulp-awspublish')
 const sass = require('gulp-sass')
 const w3cjs = require('gulp-w3cjs')
-const mustache = require("gulp-mustache");
+const mustache = require('gulp-mustache')
+const rename = require('gulp-rename')
+const clean = require('gulp-clean')
 
-gulp.task('deploy', ['sass', 'w3c-validation'], ()=> {
+gulp.task('deploy', ['w3c-validation'], ()=> {
   const publisher = awspublish.create({
     region: 'eu-west-2',
     params: {
-      Bucket: 'homepetsuk.com'
+      Bucket: 'purrsandpawsuk.com'
     },
     accessKeyId: s3_user.key,
     secretAccessKey: s3_user.secret
   })
   const headers = {
-    'Cache-Control': 'max-age=315360000, no-transform, public'
+    'Cache-Control': 'max-age=3600, no-transform, public'
   }
   return gulp.src('public/**/*')
     .pipe(publisher.publish(headers))
@@ -23,7 +25,7 @@ gulp.task('deploy', ['sass', 'w3c-validation'], ()=> {
     .pipe(awspublish.reporter())
 })
 
-gulp.task('sass', () => {
+gulp.task('sass', ['copy-fonts'], () => {
   return gulp.src(['./sass/*.scss'])
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./public/css'))
@@ -31,14 +33,33 @@ gulp.task('sass', () => {
  
 gulp.task('w3c-validation', ['build-pages'], () =>  {
   return gulp.src('public/*.html')
-        .pipe(w3cjs())
-        .pipe(w3cjs.reporter());
+    .pipe(w3cjs())
+    .pipe(w3cjs.reporter())
 })
 
-gulp.task('build-pages', () => {
-  return gulp.src("./page-templates/*.html")
-      .pipe(mustache({}))
-      .pipe(gulp.dest("./public"))
+gulp.task('build-pages', ['sass'], () => {
+  const parameters = require('./page-templates/page-parameters.json')
+
+  return gulp.src('./page-templates/*.html')
+    .pipe(mustache(parameters))
+    .pipe(rename(function (path) {
+      path.extname = ''
+    }))
+    .pipe(gulp.dest('./public'))
 })
 
-gulp.task('default', ['deploy'])
+gulp.task('copy-fonts', ['clean'], () => {
+  return gulp.src(['fonts/**/*'], { base: 'fonts' })
+    .pipe(gulp.dest('public/fonts'))
+})
+
+gulp.task('clean', () => {
+  return gulp.src('public', {read: false})
+    .pipe(clean({force: true}))
+})
+
+gulp.task('watch', function() {
+    gulp.watch(['./sass/**/*', './page-templates/**/*'], ['w3c-validation'])
+})
+
+gulp.task('default', ['w3c-validation'])
